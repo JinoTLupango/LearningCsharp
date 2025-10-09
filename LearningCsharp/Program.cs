9d91e21d9f5260aa56645bb9278aaefcce2ab064
lupangojino0604@gmail.com <lupangojino0604@gmail.com>
2025-10-09 19:43:35 +0800

    using Switch

--- LearningCsharp/Program.cs
+++ LearningCsharp/Program.cs
@@ -12,3 +12,29 @@ int age = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Your age is: " + age);
 Console.WriteLine("Username is: " + userName);
+
+int day = 4;
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+}
