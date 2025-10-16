865feb2c925389d8000edf951d9bbb483ad48c9f
lupangojino0604@gmail.com <lupangojino0604@gmail.com>
2025-10-16 16:28:12 +0800

    Csharp gamit if-else statement

--- LearningCsharp/Program.cs
+++ LearningCsharp/Program.cs
@@ -1,14 +1,47 @@
-﻿// Type your username and press enter
-Console.WriteLine("Enter username:");
+﻿using System;
 
-// Create a string variable and get user input from the keyboard and store it in the variable
-string userName = Console.ReadLine();
+class Program
+{
+    public static void Main() 
+    {
+        //nangayo og pangan
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
 
-// Print the value of the variable (userName), which will display the input value
+        //nangayo og edad
+        Console.Write("Enter your Age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
 
+        //nangayo ug paborito programming langauge
+        Console.Write("Enter your favorite programming langauge: ");
+        string langauge = Console.ReadLine();
 
-Console.WriteLine("Enter your age:");
-int age = Convert.ToInt32(Console.ReadLine());
+        // iya i desplay ang user info
+        Console.WriteLine("\n--- User Information ---");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Favorite Langauge: " + langauge);
 
-Console.WriteLine("Your age is: " + age);
-Console.WriteLine("Username is: " + userName);
+        // if else statement basi sa age
+        if (age < 13)
+        {
+            Console.WriteLine("You're still young, " + name + "! Keep learning new things!");
+        }
+        else if (age >= 13 && age < 20) 
+        {
+            Console.WriteLine("You're a teenager, " + name + "! Enjoy exploring coding!");
+        }
+        else if (age >= 20 && age < 50)
+        {
+            Console.WriteLine("You're Adult, " + name + "! Keep up the great Work!");
+        }
+        else
+        {
+            Console.WriteLine("You're senior! " + name + "! Your wisdom is inspiring!");
+        }
+
+        Console.WriteLine("\nPress any key to exit....");
+        Console.ReadLine();
+    
+    }
+}
