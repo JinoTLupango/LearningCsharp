051acd358fd8dc3549e00447e6268b7d1493ace1
lupangojino0604@gmail.com <lupangojino0604@gmail.com>
2025-10-17 09:38:48 +0800

    gamit swicth statement in csharp

--- LearningCsharp/Program.cs
+++ LearningCsharp/Program.cs
@@ -1,14 +1,74 @@
-﻿// Type your username and press enter
-Console.WriteLine("Enter username:");
+﻿using System;
 
-// Create a string variable and get user input from the keyboard and store it in the variable
-string userName = Console.ReadLine();
+class Program
+{
+    public static void Main()
+    {
+        // nangayo og pangan
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
 
-// Print the value of the variable (userName), which will display the input value
+        // nangayo og edad
+        Console.Write("Enter your Age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
 
+        // nangayo ug paborito programming language
+        Console.Write("Enter your favorite programming language: ");
+        string language = Console.ReadLine();
 
-Console.WriteLine("Enter your age:");
-int age = Convert.ToInt32(Console.ReadLine());
+        // iya i display ang user info
+        Console.WriteLine("\n--- User Information ---");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Favorite Language: " + language);
 
-Console.WriteLine("Your age is: " + age);
-Console.WriteLine("Username is: " + userName);
+        // if else statement basi sa age
+        if (age < 13)
+        {
+            Console.WriteLine("You're still young, " + name + "! Keep learning new things!");
+        }
+        else if (age >= 13 && age < 20)
+        {
+            Console.WriteLine("You're a teenager, " + name + "! Enjoy exploring coding!");
+        }
+        else if (age >= 20 && age < 50)
+        {
+            Console.WriteLine("You're an adult, " + name + "! Keep up the great work!");
+        }
+        else
+        {
+            Console.WriteLine("You're a senior, " + name + "! Your wisdom is inspiring!");
+        }
+
+        Console.WriteLine("\n --- Programing langauge Message ---");
+        switch(language.ToLower())
+        {
+            case "C#":
+                Console.WriteLine("Nice! C# is a powerful for building desktop and web app!");
+                break;
+
+            case "Java":
+                Console.WriteLine("Java is a great for enterpise system and android development!");
+                break;
+
+            case "Python":
+                Console.WriteLine("Python is awesome for  AI and data science!");
+                break;
+
+            case "PHP":
+                Console.WriteLine("PHP is used for web development!");
+                break;
+
+            case "Javascript":
+                Console.WriteLine("Javascript is makes website!");
+                break;
+
+            default:
+                Console.WriteLine("That is great langauge, " + name + "!keep coding!");
+                break;
+               
+        }
+        Console.WriteLine("\n Press any key to exti...!");
+        Console.ReadLine();
+    }
+}
